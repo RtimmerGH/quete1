cd2915717ad7d5753977f10d4d55cb2d7891a792
RTimmerGH <timmer.romain@gmail.com>
2023-09-20 17:37:49 +0200

    Quete Angular 05 - Les formulaires: templates driven forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserDetails } from './user.model';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  user: UserDetails = new UserDetails("","","","")
 
+  onSubmit(){
+    console.log('user: ',this.user);    
+  }
 }
