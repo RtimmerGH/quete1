3afa1d2608e684891ef804b2883f94050a8e3ff7
RTimmerGH <timmer.romain@gmail.com>
2023-09-20 11:54:44 +0200

    Quete Angular 04 - Le Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,    
+    RouterModule.forRoot(ROUTES),    
   ],
   providers: [],
   bootstrap: [AppComponent]
